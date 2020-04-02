fe72f8386ad3d21ef7a505bf7815108c8a8d15e6
Kizito Njoku <nkizito57@gmail.com>
2020-04-02 02:36:01 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,12 @@
 // Code ClickityClick Component Here
+import React, {Component} from 'react'
+
+export default class ClickityClick extends Component { 
+    
+    render() {
+      return (
+        <div />
+        )
+    }
+  
+}
