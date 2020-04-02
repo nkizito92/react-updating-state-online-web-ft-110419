e015d5097ba096271e7b479d10a3770971da43da
Kizito Njoku <nkizito57@gmail.com>
2020-04-02 02:57:37 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -17,6 +17,7 @@ export default class ClickityClick extends Component {
     render() {
       return (
         <div>
+          <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
           <button onClick={this.handleClick}>Click me!</button>
         </div>
         );
